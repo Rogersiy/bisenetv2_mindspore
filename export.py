a43937e9dc42fb82e9093396d999ae9fff4cb4f1
rogersiy <949206904@qq.com>
2023-07-29 11:32:05 +0800

    添加导出
    添加导出

--- export.py
+++ export.py
@@ -1,35 +1,50 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Export checkpoint into mindir or air for 310 inference."""
+import argparse
+
 import numpy as np
-import mindspore as ms
-from mindspore import ops, nn
+from mindspore import Tensor, context, load_checkpoint, load_param_into_net, export
+
+from src.modules.bisenetv2 import BiSeNetV2
 
 
-def cross_entropy(logits, labels, weight, ignore_label=255):
-    n, c = logits.shape
-    logits = ops.softmax(logits)
-    labels_mask = ops.logical_and(labels != ignore_label, labels >= 0)
-    labels = ops.select(labels_mask, labels, ops.zeros_like(labels))
-    labels = ops.one_hot(labels, c, ms.Tensor(1.0, ms.float32), ms.Tensor(0.0, ms.float32))
-    labels = labels.astype(logits.dtype)
-    weight = weight.reshape((1, -1)).astype(logits.dtype)
-    weight = ops.stop_gradient(ops.tile(weight, (n, 1)))
-    loss = ops.binary_cross_entropy(logits, labels, weight, reduction='none')
-    labels_mask = ops.tile(labels_mask.reshape(n, 1), (1, c))
-    loss = loss * labels_mask
-    return loss.sum(-1).mean()
+def main():
+    """Export process."""
+    parser = argparse.ArgumentParser("BiSeNetv2 Semantic Segmentation exporting.")
+    parser.add_argument("--device_id", type=int, default=0, help="Device ID. ")
+    parser.add_argument("--device_target", type=str, default="Ascend", help="Target device [Ascend, GPU]")
+    parser.add_argument("--checkpoint_file", type=str, help="Checkpoint file path. ")
+    parser.add_argument("--file_name", type=str, help="Output file name. ")
+    parser.add_argument("--file_format", type=str, default="MINDIR",
+                        choices=["AIR", "MINDIR"], help="Output file format. ")
 
-logits = ms.Tensor(np.array([[3, 5, 6, 9, 12, 33, 42, 12, 32, 72],
-                             [3, 5, 6, 9, 12, 33, 42, 12, 32, 72],
-                             [3, 5, 6, 9, 12, 33, 42, 12, 32, 72]]), ms.float32)
-labels = ms.Tensor(np.array([1, 5, 255]).astype(np.int32))
-print(ops.tile(labels.reshape(1, 3), (2, 1)))
-# depth, on_value, off_value = 10, ms.Tensor(1.0, ms.float32), ms.Tensor(0.0, ms.float32)
-# labels_onehot = ops.one_hot(labels, depth, on_value, off_value)
-# weight = ops.arange(10).astype(ms.float32) * 0.1
-#
-# ce = nn.CrossEntropyLoss(weight=weight, ignore_index=255, reduction="mean")
-#
-# loss1 = ce(logits, labels)
-# print(loss1)
-# loss2 = cross_entropy(logits, labels, weight, 255)
-# print(loss2)
+    args = parser.parse_args()
 
+    context.set_context(mode=context.GRAPH_MODE, device_target=args.device_target, device_id=args.device_id)
+    
+    net=BiSeNetV2(n_classes=19,aux_mode='eval', backbone_url='')
+    
+    params_dict = load_checkpoint(args.checkpoint_file)
+    load_param_into_net(net, params_dict)
+    net.set_train(False)
+    height, width = 1024, 2048
+    input_data = Tensor(np.zeros([1, 3, height, width], dtype=np.float32))
+    export(net, input_data, file_name=args.file_name, file_format=args.file_format)
+
+
+if __name__ == "__main__":
+    main()
